19c55990d54e1d24c8b9dceba9077ed651773bc8
Art5019 <60709850+Arthur-Pereira-5019@users.noreply.github>
2025-03-31 08:05:46 -0300

    Second upload

--- Atividades Telas/src/art5019/telas/um/Tela1.java
+++ Atividades Telas/src/art5019/telas/um/Tela1.java
@@ -1,12 +1,23 @@
 package art5019.telas.um;
 
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
+import javax.swing.JLabel;
 
 public class Tela1 {
 
 	private JFrame frame;
+	private JTextField campoNome;
+	private JLabel lblNewLabel;
+	private JLabel mostrarNome;
 
 	/**
 	 * Launch the application.
@@ -38,6 +49,31 @@ public class Tela1 {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		campoNome = new JTextField();
+		campoNome.setBounds(129, 60, 86, 20);
+		frame.getContentPane().add(campoNome);
+		campoNome.setColumns(10);
+		
+		JButton btnNewButton = new JButton("MOSTRAR");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				mostrarNome.setText(campoNome.getText());
+			
+			}
+		});
+		btnNewButton.setBounds(165, 161, 89, 23);
+		frame.getContentPane().add(btnNewButton);
+		
+		lblNewLabel = new JLabel("Nome:");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		lblNewLabel.setBounds(10, 45, 109, 54);
+		frame.getContentPane().add(lblNewLabel);
+		
+		mostrarNome = new JLabel("");
+		mostrarNome.setBounds(33, 189, 46, 14);
+		frame.getContentPane().add(mostrarNome);
 	}
-
 }
